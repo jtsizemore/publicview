cbf694f024b7af5476ef17647c79c1be4716c2b7
jtsizemore <77204788+jtsizemore@users.noreply.github.com>
2022-08-01 20:51:55 -0400

    Update resume_info.py
    update

--- podmanflaskdev/resume_info.py
+++ podmanflaskdev/resume_info.py
@@ -17,92 +17,179 @@ senior = 'Network operations'
 bbt_desc = 'Network operations'
 
 
-work_history = {
-    12: {
-        'id': 12,
+# work_history = {
+#     12: {
+#         'id': 12,
+#         'company': metlife,
+#         'date': '9/21 - Current',
+#         'role': 'Principal Network Engineer',
+#         'description': principal
+#     },
+#     11: {
+#         'id': 11,
+#         'company': metlife,
+#         'date': '3/20 - 9/21',
+#         'role': 'Lead Network Engineer',
+#         'description': lead
+#     },
+#     10: {
+#         'id': 10,
+#         'company': metlife,
+#         'date': '6/16 - 3/20',
+#         'role': 'Senior Network Analyst',
+#         'description': senior
+#     },
+#     9: {
+#         'id': 9,
+#         'company': 'BB&T Bank - Wilson, NC',
+#         'date': '3/15 - 6/16',
+#         'role': 'Senior Analyst III Network Production Engineering',
+#         'description': bbt_desc
+#     },
+#     8: {
+#         'id': 8,
+#         'company': 'BB&T Bank / Insight Global - Wilson, NC',
+#         'date': '9/13 - 3/15',
+#         'role': 'Contractor - Senior Analyst III Network Production Engineering',
+#         'description': bbt_desc
+#     },
+#     7: {
+#         'id': 7,
+#         'company': 'Cisco Systems / Apex Systems Inc. - RTP, NC',
+#         'date': '8/12 - 9/13',
+#         'role': 'Contractor - Tier 2/Shift Lead - Cisco Remote Management Services (RMS)',
+#         'description': 'Network operations'
+#     },
+#     6: {
+#         'id': 6,
+#         'company': 'Proactive Communications Inc. - Afghanistan',
+#         'date': '2/12 - 8/12',
+#         'role': 'VSAT Engineer - Junior/IT Support',
+#         'description': 'VSAT, network operations, and IT technical support'
+#     },
+#     5: {
+#         'id': 5,
+#         'company': vzb,
+#         'date': '1/10 - 2/12',
+#         'role': 'Specialist Network Operations - Customer Managed Services',
+#         'description': 'Manage/maintain/troubleshoot customer LAN/WAN networks'
+#     },
+#     4: {
+#         'id': 4,
+#         'company': vzb,
+#         'date': '11/08 - 1/10',
+#         'role': 'Advanced Technician - Global Data Network Operations',
+#         'description': 'Manage/maintain/troubleshoot Verizon global networks'
+#     },
+#     3: {
+#         'id': 3,
+#         'company': 'Time Warner Cable - Greensboro, NC',
+#         'date': '5/08 - 11/08',
+#         'role': 'Business Class Tier 3 Customer Support Engineer',
+#         'description': 'Tier 3 network and technical support for TWC Business Class customers'
+#     },
+#     2: {
+#         'id': 2,
+#         'company': 'Wake Technical Community College - Raleigh, NC',
+#         'date': '1/06 - 5/08',
+#         'role': 'Student',
+#         'description': 'Networking Technologies Certificate Program'
+#     },
+#     1: {
+#         'id': 1,
+#         'company': 'United States Air Force',
+#         'date': '4/97 - 8/04',
+#         'role': 'Staff Sergeant US Air Force - 1N374G Far East Cryptologic Linguist - Korean',
+#         'description': 'Cryptologic Linguist'
+#     },
+# }
+
+work_history = [
+    {
+        # 'id': 12,
         'company': metlife,
         'date': '9/21 - Current',
         'role': 'Principal Network Engineer',
         'description': principal
     },
-    11: {
-        'id': 11,
+    {
+        # 'id': 11,
         'company': metlife,
         'date': '3/20 - 9/21',
         'role': 'Lead Network Engineer',
         'description': lead
     },
-    10: {
-        'id': 10,
+    {
+        # 'id': 10,
         'company': metlife,
         'date': '6/16 - 3/20',
         'role': 'Senior Network Analyst',
         'description': senior
     },
-    9: {
-        'id': 9,
+    {
+        # 'id': 9,
         'company': 'BB&T Bank - Wilson, NC',
         'date': '3/15 - 6/16',
         'role': 'Senior Analyst III Network Production Engineering',
         'description': bbt_desc
     },
-    8: {
-        'id': 8,
+    {
+        # 'id': 8,
         'company': 'BB&T Bank / Insight Global - Wilson, NC',
         'date': '9/13 - 3/15',
         'role': 'Contractor - Senior Analyst III Network Production Engineering',
         'description': bbt_desc
     },
-    7: {
-        'id': 7,
+    {
+        # 'id': 7,
         'company': 'Cisco Systems / Apex Systems Inc. - RTP, NC',
         'date': '8/12 - 9/13',
         'role': 'Contractor - Tier 2/Shift Lead - Cisco Remote Management Services (RMS)',
         'description': 'Network operations'
     },
-    6: {
-        'id': 6,
+    {
+        # 'id': 6,
         'company': 'Proactive Communications Inc. - Afghanistan',
         'date': '2/12 - 8/12',
         'role': 'VSAT Engineer - Junior/IT Support',
         'description': 'VSAT, network operations, and IT technical support'
     },
-    5: {
+    {
         'id': 5,
         'company': vzb,
         'date': '1/10 - 2/12',
         'role': 'Specialist Network Operations - Customer Managed Services',
         'description': 'Manage/maintain/troubleshoot customer LAN/WAN networks'
     },
-    4: {
-        'id': 4,
+    {
+        # 'id': 4,
         'company': vzb,
         'date': '11/08 - 1/10',
         'role': 'Advanced Technician - Global Data Network Operations',
         'description': 'Manage/maintain/troubleshoot Verizon global networks'
     },
-    3: {
-        'id': 3,
+    {
+        # 'id': 3,
         'company': 'Time Warner Cable - Greensboro, NC',
         'date': '5/08 - 11/08',
         'role': 'Business Class Tier 3 Customer Support Engineer',
         'description': 'Tier 3 network and technical support for TWC Business Class customers'
     },
-    2: {
-        'id': 2,
+    {
+        # 'id': 2,
         'company': 'Wake Technical Community College - Raleigh, NC',
         'date': '1/06 - 5/08',
         'role': 'Student',
         'description': 'Networking Technologies Certificate Program'
     },
-    1: {
-        'id': 1,
+    {
+        # 'id': 1,
         'company': 'United States Air Force',
         'date': '4/97 - 8/04',
         'role': 'Staff Sergeant US Air Force - 1N374G Far East Cryptologic Linguist - Korean',
         'description': 'Cryptologic Linguist'
     },
-}
+]
 
 security_clearance = {
     'afghanistan': 'Favorable NACI (National Agency Check with Inquiries)',
